ea393604ca7d9f48168002a210e13eb4ccb5dfb4
Paul Bailey <paul.m.bailey@gmail.com>
2017-08-30 22:10:46 +0000

    better backup

--- pytx/views.py
+++ pytx/views.py
@@ -1,8 +1,18 @@
+import os
 import subprocess
 
 from django import http
 
 def backup (request):
-  sts = subprocess.call("./upload_db.bsh", shell=True)
-  return http.HttpResponse("OK", content_type="text/plain")
+  token = request.GET.get('token', '')
+  ENVIRONMENT = os.environ.get('ENVIRONMENT', '')
+  
+  if token == os.environ.get('BACKUP_TOKEN', ''):
+    if ENVIRONMENT == 'production':
+      sts = subprocess.call("./upload_db.bsh", shell=True)
+      
+    return http.HttpResponse("OK", content_type="text/plain")
+    
+  return http.HttpResponse("NOT-OK", content_type="text/plain")
+  
   
