39763fcc33c97463f17d9ba6e4a2789122e556cc
Paul Bailey <paul.m.bailey@gmail.com>
2017-10-03 22:30:42 +0000

    moving submodule to package

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,54 @@
-frontend/gulpfile.js
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var concat = require("gulp-concat");
+var less = require('gulp-less');
+var rollup = require('rollup').rollup;
+var buble = require('rollup-plugin-buble');
+var resolve = require('rollup-plugin-node-resolve');
+var commonjs = require('rollup-plugin-commonjs');
+
+var build_tasks = ['build-js', 'build-css', 'copy-md'];
+
+gulp.task('build-js', function () {
+  return rollup({
+    input: './frontend/app/pytx.js',
+    plugins: [
+      resolve({ jsnext: true }),
+      commonjs(),
+      buble()
+    ],
+    external: ['vue', 'vue-router', 'vue-material']
+  }).then(function (bundle) {
+    return bundle.write({
+      format: 'iife',
+      file: './frontend/2017-dist/pytx.js',
+      globals: {
+        "vue": 'Vue',
+        "vue-router": 'VueRouter',
+        "vue-material": 'VueMaterial'
+      },
+    });
+  });
+});
+
+gulp.task('build-css', function () {
+  return gulp.src("frontend/app/**/*.less")
+    .pipe(plumber())
+    .pipe(less({paths: ['frontend/less']}))
+    .pipe(concat('pytx.css'))
+    .pipe(gulp.dest("frontend/2017-dist"));
+});
+
+gulp.task('copy-md', function () {
+  return gulp.src("frontend/app/md/**/*.md")
+    .pipe(plumber())
+    .pipe(gulp.dest("frontend/2017-dist/md"));
+});
+
+gulp.task('watch', build_tasks, function () {
+  gulp.watch("frontend/app/md/**/*.md", ['copy-md']);
+  gulp.watch("frontend/app/**/*.js", ['build-js']);
+  gulp.watch("frontend/**/*.less", ['build-css']);
+});
+
+gulp.task('default', build_tasks);
