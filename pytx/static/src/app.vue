60204b85296a1981978160cf96091d358bc38340
Paul Bailey <paul.m.bailey@gmail.com>
2019-03-12 18:27:18 -0500

    fix link

--- pytx/static/src/app.vue
+++ pytx/static/src/app.vue
@@ -84,7 +84,7 @@
           <v-list-tile to="/page/about/conference">
             <v-list-tile-title>About the Conference</v-list-tile-title>
           </v-list-tile>
-          <v-list-tile to="/page/about/volunteer">
+          <v-list-tile to="/page/about/volunteering">
             <v-list-tile-title>Volunteer Opportunities</v-list-tile-title>
           </v-list-tile>
           <v-list-tile to="/page/about/grants">
